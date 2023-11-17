a0a8fdc8299f6168a3afccd684998d522413860a
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-17 12:49:22 +0100

    no message

--- src/edu/ejercicio3bucleED/ejercicio3bucleEd.java
+++ src/edu/ejercicio3bucleED/ejercicio3bucleEd.java
@@ -8,17 +8,16 @@ public class ejercicio3bucleEd
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		int a = 100 * (int) Math.random();
-		int b;
+		int a = (int)(Math.random()*100);
+		int b= 0;
 		System.out.println("escribe un numero entre 0 y 100");
 		int elegido =scan.nextInt();
 		for (int i = 1; i < 11 ; i++) 
 		{
 			if ( elegido < a ) {System.out.println("es mayor");}
 			else if ( elegido > a ) {System.out.println("es menor");}
-			else (a == elegido){System.out.println("Acertastes");}
-			 b = 1+b;
-			System.out.println("acertastes en  " + b + "  veces");
-		}
+			else if (a == elegido){System.out.println("Acertastes");}
+			 b++;
+		}System.out.println("acertastes en  " + b + "  veces");
 	}
 }
